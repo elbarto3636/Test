d3604a340f16e2a45531209fcf6d2a88f9f882bb
elbarto3636 <racetraver@gmail.com>
2016-02-28 21:16:29 -0500

    Added files via upload

---
+++ wave.java
@@ -0,0 +1,23 @@
+
+public class wave {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(system.in);
+		
+		
+		System.out.println("Input es ");
+			
+	}
+
+}
+
+public class board {
+	
+	public void duder(int ying){
+		System.out.println("integer")
+	}
+	
+	public void duder(char yang){
+		system.out.println("word")
+	}
+}
